30e6731a844e52cdcf5dcd277c894e7d50e06862
IllyaZhugan <jugan.i@icloud.com>
2025-04-30 14:24:08 +0300

    homwork3.3

---
+++ hm3.3.py
@@ -0,0 +1,15 @@
+def split_list(lst):
+
+    if not lst:
+        return [[], []]
+
+    middle = (len(lst) + 1) // 2
+
+    return [lst[:middle], lst[middle:]]
+
+
+print(split_list([1, 2, 3, 4, 5, 6]))  # [[1, 2, 3], [4, 5, 6]]
+print(split_list([1, 2, 3]))  # [[1, 2], [3]]
+print(split_list([1, 2, 3, 4, 5]))  # [[1, 2, 3], [4, 5]]
+print(split_list([1]))  # [[1], []]
+print(split_list([]))  # [[], []]
