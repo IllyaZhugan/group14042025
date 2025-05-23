187ce76551ce2c739e556bf72065db8f952eabd7
IllyaZhugan <jugan.i@icloud.com>
2025-05-23 08:40:52 +0300

    hw9.1

--- hm9_1.py
+++ hm9_1.py
@@ -8,7 +8,5 @@ def popular_words(text, words):
 assert popular_words(
     """When I was One I had just begun When I was Two I was nearly new""",
     ["i", "was", "three", "near"],
-) == {"i": 4, "was": 3, "three": 0, "near": 0},
-
+) == {"i": 4, "was": 3, "three": 0, "near": 0}
 print("OK")
-
