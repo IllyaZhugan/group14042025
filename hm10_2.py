ad65e097c548fcc97a219a6ca838d5166bce6921
IllyaZhugan <jugan.i@icloud.com>
2025-05-26 20:17:19 +0300

    hm10.2

--- hm10_2.py
+++ hm10_2.py
@@ -1,5 +1,6 @@
 import re
 
+
 def first_word(text):
     match = re.search(r"[a-zA-Z']+", text)
     if match:
@@ -11,8 +12,6 @@ assert first_word("Hello world") == "Hello"
 assert first_word("greetings, friends") == "greetings"
 assert first_word("don't touch it") == "don't"
 assert first_word(".., and so on ...") == "and"
-assert first_word("hi") == "hi", 'Test5'
+assert first_word("hi") == "hi", "Test5"
 assert first_word("Hello.World") == "Hello"
-print('OK')
-
-
+print("OK")
